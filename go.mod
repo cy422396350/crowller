262360b31e3d29a56a2b40add184b71f1e0b9e83
常岳 <422396350@qq.com>
2019-12-24 09:56:11 +0800

    存储到elastic search

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
 	golang.org/x/text v0.3.0
+	gopkg.in/olivere/elastic.v5 v5.0.83
 )
--- itemServer/createItemServer.go
+++ itemServer/createItemServer.go
@@ -1,7 +1,9 @@
 package itemServer
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"gopkg.in/olivere/elastic.v5"
 )
 
 func CreateItemServer() chan interface{} {
@@ -9,8 +11,24 @@ func CreateItemServer() chan interface{} {
 	go func() {
 		for {
 			item := <-out
-			log.Println("item is ", item)
+			save(item)
 		}
 	}()
 	return out
+}
+
+func save(item interface{}) {
+	client, err := elastic.NewClient(
+		elastic.SetURL("http://47.104.141.245:9200"),
+		//false in docker
+		elastic.SetSniff(false),
+	)
+	if err != nil {
+		panic(err)
+	}
+	response, err := client.Index().Index("data_profile").Type("zhenai").BodyJson(item).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(response)
 }
